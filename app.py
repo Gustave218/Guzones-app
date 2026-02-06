2a972900e54de3f785134bc3db0b426ceb602410
Gustave Shumbusho <gustaveshumbusho17@gmail.com>
2026-02-06 16:11:40 +0300

    Fix postgress driver

--- app.py
+++ app.py
@@ -106,6 +106,9 @@ app.config.from_object(Config)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 if DATABASE_URL:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 else:
